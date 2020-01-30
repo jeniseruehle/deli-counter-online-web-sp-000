83ec2ba29df87dfd37cb447ecb268c81f5f94fe0
Jenise Ruehle <jela07@gmail.com>
2020-01-30 15:06:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,6 +10,10 @@ def line(katz_deli)
   end
 end
 
+def take_a_number(katz_deli, customer)
+  katz_deli.push(customer)
+    puts "Welcome, #{customer}. You are number #{katz_deli.length} in line."
+end 
 
 
 def now_serving 
