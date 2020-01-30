9dde4c9f3fb2c4c57226752d6ffaf9b8ccf22072
Jenise Ruehle <jela07@gmail.com>
2020-01-30 15:20:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,10 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
-  elsif katz_deli.length == (> 0)
+  elsif
     katz_deli.each_with_index do |customer, index|
-      
-    
+      puts "The line is currently: #{index}. #{customer}" if customer > 0
+    end   
   end   
 end
 
