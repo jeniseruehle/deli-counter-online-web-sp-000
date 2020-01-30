b012bee50be0b560d85ac407636adeef330c476f
Jenise Ruehle <jela07@gmail.com>
2020-01-30 15:07:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,12 +2,8 @@ katz_deli = []
 
 def line(katz_deli)
   if katz_deli.length == 0 
-     puts "The line is currently empty"
-  else   
-    katz_deli.each_with_index do |customer, index|
-      puts "The line is currently: #{index.to_i+1}. #{customer}"
-    end
-  end
+    puts "The line is currently empty."
+  end   
 end
 
 def take_a_number(katz_deli, customer)
