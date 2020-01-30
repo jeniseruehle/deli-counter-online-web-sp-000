b6a6976cbab9b2bd7cc7f946fa6a199a90ef7cbe
Jenise Ruehle <jela07@gmail.com>
2020-01-30 15:09:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,9 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
+  elsif katz_deli.length == (> 0)
+    katz_deli.each_with_index do 
+    
   end   
 end
 
